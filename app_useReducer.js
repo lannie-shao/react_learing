d48d22a429ba264637b6b53eeaf7210a6e8ec05c
Lannie Shao <lannie.shao@gmail.com>
2022-08-18 18:53:36 +1000

    when to use Reducer hook

---
+++ app_useReducer.js
@@ -0,0 +1,22 @@
+import './App.css';
+import {useReducer} from 'react'
+
+
+function App() {
+  //useReducer instead of use State because we use logic function here
+  //to set the state
+  const [checked,setChecked]=useReducer(checked=>!checked,false);
+
+
+  return (   
+     <div className='App'>
+       <input type='checkbox'  
+       value={checked}
+       onChange={setChecked}
+       />
+       <label>{checked?'checked':'not checked'}</label>
+    </div>
+  );
+}
+
+export default App;
