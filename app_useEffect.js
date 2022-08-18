808512b99ae8e7078cd02636d547851610be99ec
Lannie Shao <lannie.shao@gmail.com>
2022-08-18 12:29:40 +1000

    use object as initial state

--- app_useEffect.js
+++ app_useEffect.js
@@ -6,16 +6,27 @@ function App() {
 //use a hook
 const [emotion,setEmotion]=useState('happy');
 const [secondary, setSecondary]=useState('peaceful')
+const [car,setCar]=useState({
+  model:"Mustang ",
+  brand:"Ford",
+  year:'1964',
+  color:'yellow'
+});
 
 //effect once
 useEffect(()=>{
   console.log(`It's ${emotion} right now.`);
 },[]);
 
-//effect will happen every time the emotion changed
+//effect will happen every emotion changed
 useEffect(()=>
 console.log(`It's ${secondary} around here.`),[emotion]
 );
+
+useEffect(()=>
+console.log(`It's a ${car.color} ${car.brand} of ${car.year}`),[car]
+);
+
   return (   
     <div>
       <h1>Hello from React.</h1>
@@ -23,6 +34,8 @@ console.log(`It's ${secondary} around here.`),[emotion]
       <button onClick={()=>setEmotion("excited")}>Excited</button>
       <button onClick={()=>setEmotion("tired")}>Tired</button>
       <h3>It's {secondary} around here.</h3>
+      <p>I have a {car.color} car.</p>
+      <button onClick={()=>setCar({color:"red",brand:'Holden', year:2022})}>Red</button>
     </div>
   );
 }
